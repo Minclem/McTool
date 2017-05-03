c7a950aa2b89a0155ac96aeb9fdb32aaa40380f1
Minje <MingJie-Wang@users.noreply.github.com>
2017-05-03 16:00:29 +0800

    Update event.js

--- src/event.js
+++ src/event.js
@@ -3,17 +3,17 @@ var Event = function () {};
 Event.prototype.addEvent = function (target, eventType, handle) {
     if (document.addEventListener) {
         Event.addEvent = function (target, eventType, handle) {
-            target.addEventListener(eventType,handle,false);
+            target.addEventListener(eventType, handle, false);
         }
     } else if (document.attachEvent) {
-        Event.addEvent = function(target,eventType,handle){
-            target.attachEvent('on'+eventType, function () {
-                handle.call(target,arguments);
+        Event.addEvent = function(target, eventType, handle){
+            target.attachEvent('on' + eventType, function () {
+                handle.call(target, arguments);
             });
         }
     } else {
-        Event.addEvent = function (target,eventType,handle) {
-            target['on'+eventType] = handle;
+        Event.addEvent = function (target, eventType, handle) {
+            target['on' + eventType] = handle;
         }
     }
     Event.addEvent(target, eventType, handle);
